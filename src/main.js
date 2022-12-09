932bd3657c295e3cc0b53c56c18fce25a6f2dcff
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2022-12-09 10:19:55 -0300

    Revert "adição de bootstrapvue e iconsplugin, criação da rota e da view dashboard"
    This reverts commit a5e7c87dda239181c385d4f39427b2635569b057.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import './assets/main.css'
-
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-// Make BootstrapVue available throughout your project
-app.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-app.use(IconsPlugin)
 
 const app = createApp(App)
 
