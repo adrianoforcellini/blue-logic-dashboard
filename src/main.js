a5e7c87dda239181c385d4f39427b2635569b057
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2022-12-09 10:04:51 -0300

    adição de bootstrapvue e iconsplugin, criação da rota e da view dashboard

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import './assets/main.css'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+app.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+app.use(IconsPlugin)
 
 const app = createApp(App)
 
