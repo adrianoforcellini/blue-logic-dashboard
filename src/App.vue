74251363807273d6c6f845df864eabbdc6724d52
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2022-12-09 12:44:07 -0300

    separação de todas as grids e criação de todos os cards

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,55 @@ import Tractor from "vue-material-design-icons/Tractor.vue";
     <div class="left-content">
       <section class="section-top">
         <div
-          class="cards"
-          style="width: 5%; display: flex; flex-direction: column;padding:5px"
+          class="card"
+          style="
+            width: 5%;
+            display: flex;
+            flex-direction: column;
+            padding: 5px;
+            border-radius: 30px;
+          "
         >
-          <Tractor style="color: white; margin-left: 10%; margin-top:5%" :size="40" />
-          <h1 style="margin-left:20%">40</h1>
+          <Tractor
+            style="color: white; margin-left: 5%; margin-top: 25%"
+            :size="38"
+          />
+          <h1 style="margin-left: 20%">40</h1>
         </div>
-        <div class="cards" style="width: 70%"></div>
-        <div class="cards" style="width: 20%"></div>
+        <div class="card" style="width: 70%; border-radius: 25px"></div>
+        <div class="card" style="width: 20%; border-radius: 25px"></div>
       </section>
-      <section class="section-bottom"></section>
+      <section class="section-bottom">
+        <div class="bottom-left">
+          <div class="card" style="width: 90%; border-radius: 35px"></div>
+        </div>
+        <div class="bottom-rigth">
+          <div class="top">
+            <div class="card" style="width: 50%; border-radius: 50px"></div>
+            <div class="card" style="width: 50%; border-radius: 50px"></div>
+          </div>
+          <div class="bottom">
+            <div
+              class="card"
+              style="border-radius: 50px; margin-top: 15px"
+            ></div>
+          </div>
+        </div>
+      </section>
     </div>
-    <div class="right-content"></div>
+    <div class="right-content">
+      <div class="card" style="border-radius:30px"></div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+
+.right-content {
+  width: 30vw;
+  padding:15px
+}
+
 .left-content {
   width: 70vw;
   height: 100vh;
@@ -41,6 +74,7 @@ import Tractor from "vue-material-design-icons/Tractor.vue";
   flex-direction: row;
 }
 .left-content .section-bottom {
+  display: flex;
   width: 100%vw;
   height: 75vh;
   justify-content: space-around;
@@ -48,33 +82,50 @@ import Tractor from "vue-material-design-icons/Tractor.vue";
   flex-direction: row;
 }
 
-.right-content {
-  width: 30vw;
-  height: 100vh;
+.section-bottom .bottom-left {
+  width: 30%;
+  height: 90%;
 }
 
+.section-bottom .bottom-rigth {
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  height: 100%;
+}
+
+.bottom-rigth .top {
+  display: flex;
+  width: 100%;
+  height: 40%;
+}
+
+.bottom-rigth .bottom {
+  width: 100%;
+  height: 40%;
+}
 .background {
   display: flex;
   background-color: rgb(67, 79, 91);
-  color:white
+  color: white;
 }
 
-.cards {
+.card {
+  display: flex;
   background-color: rgb(56, 58, 70);
   height: 100%;
-  border-radius: 25%;
   margin-left: 10px;
   margin-right: 10px;
 }
 
-.cards .orange-one {
+.card .orange-one {
   background-color: rgb(239, 91, 1);
 }
-.cards .orange-two {
+.card .orange-two {
   background-color: rgb(175, 80, 24);
 }
 
-.cards .orange-three {
+.card .orange-three {
   background-color: rgb(82, 62, 55);
 }
 </style>
